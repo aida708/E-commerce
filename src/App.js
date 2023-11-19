024f66005fd1c3df5a8d9622d8bbb6e891fda86d
Aida <aidasufaj00@gmail.com>
2023-11-19 18:47:50 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const Button = styled.button`
-  background: red;
-  color: white;
-`;
-const Container = styled.h5`
-  background: red;
-  color: black;
-  border: 1rem;
-
-  .w {
-    font-size: 6rem;
-    color: blue;
-  }
-`;
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
   return (
-    <div>
-      <Button>click me</Button>
-      <h4>comfy sloth starter</h4>
-      <Container>
-        <button className="w">kliko</button>
-        <h5>Hello Aida</h5>
-      </Container>
-    </div>
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
