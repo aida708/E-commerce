d2326ca5376fb5ccaee79da951be0f524d6266c0
Aida <aidasufaj00@gmail.com>
2023-11-19 13:07:42 +0100

    styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: red;
+  color: white;
+`;
+const Container = styled.h5`
+  background: red;
+  color: black;
+  border: 1rem;
+
+  .w {
+    font-size: 6rem;
+    color: blue;
+  }
+`;
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <div>
+      <Button>click me</Button>
+      <h4>comfy sloth starter</h4>
+      <Container>
+        <button className="w">kliko</button>
+        <h5>Hello Aida</h5>
+      </Container>
+    </div>
+  );
 }
 
-export default App
+export default App;
