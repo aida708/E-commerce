28bdeaa06b32020db7f6ac578285a87b69b24d6d
Aida <aidasufaj00@gmail.com>
2023-11-20 12:30:52 +0100

    #products context setup

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,21 +9,25 @@ import CartButtons from "./CartButtons";
 import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
-  const isOpen = true;
+  const { isSidebarOpen, closeSidebar } = useProductsContext();
   return (
     <SidebarContainer>
-      <aside className={`${isOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <aside
+        className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}
+      >
         <div className="sidebar-header">
           <img src={logo} className="logo" alt="comfy-sloth" />
-          <button className="close-btn" type="button">
+          <button className="close-btn" type="button" onClick={closeSidebar}>
             <FaTimes />
           </button>
         </div>
         <ul className="links">
           {links.map(({ id, text, url }) => {
             return (
-              <li>
-                <Link to="./checkout">checkout</Link>
+              <li key={id}>
+                <Link to={url} onClick={closeSidebar}>
+                  {text}
+                </Link>
               </li>
             );
           })}
