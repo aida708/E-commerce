9eea2d9eee2244fd96818fd5ca971604aa53713c
Aida <aidasufaj00@gmail.com>
2023-11-21 17:02:15 +0100

    #stars (array approach)

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -3,60 +3,24 @@ import styled from "styled-components";
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 const Stars = ({ stars, reviews }) => {
   console.log(stars, reviews);
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5;
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <BsStarFill />
+        ) : stars >= number ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    );
+  });
   return (
     <Wrapper>
-      <div className="stars">
-        <p className="reviews">{reviews} customer reviews</p>
-        {/* star */}
-        <span>
-          {stars >= 1 ? (
-            <BsStarFill />
-          ) : stars >= 0.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end of the star */}
-        <span>
-          {stars >= 2 ? (
-            <BsStarFill />
-          ) : stars >= 1.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-
-        <span>
-          {stars >= 3 ? (
-            <BsStarFill />
-          ) : stars >= 2.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        <span>
-          {stars >= 4 ? (
-            <BsStarFill />
-          ) : stars >= 3.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-
-        <span>
-          {stars >= 5 ? (
-            <BsStarFill />
-          ) : stars >= 4.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-      </div>
+      <div className="stars"> {tempStars}</div>
+      <p className="reviews">{reviews} customer reviews</p>
     </Wrapper>
   );
 };
