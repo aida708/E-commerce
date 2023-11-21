c5a29a0bd073e2e6ff82990adc4a7f5cd21a7d02
Aida <aidasufaj00@gmail.com>
2023-11-21 16:08:54 +0100

    stars

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -29,13 +29,13 @@ const SingleProductPage = () => {
   }, [id]);
 
   //return back to the homepage after 3s if there is an error
-  // useEffect(() => {
-  //   if (error) {
-  //     setTimeout(() => {
-  //       history.push("/");
-  //     }, 3000);
-  //   }
-  // }, [error]);
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        history.push("/");
+      }, 3000);
+    }
+  }, [error]);
 
   if (loading) {
     return <Loading />;
@@ -67,7 +67,7 @@ const SingleProductPage = () => {
           <ProductImages images={images} />
           <section className="content">
             <h2>{name}</h2>
-            <Stars />
+            <Stars stars={stars} reviews={reviews} />
             <h5 className="price">{formatPrice(price)}</h5>
             <p className="desc">{description}</p>
             <p className="info">
