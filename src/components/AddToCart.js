0bef927dac510d3ada60925ccc021a93feca716e
Aida <aidasufaj00@gmail.com>
2023-11-24 11:06:57 +0100

    worked with increase and decrease btns

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -10,8 +10,24 @@ const AddToCart = ({ product }) => {
   const [mainColor, setMainColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
 
-  const increase = () => {};
-  const decrease = () => {};
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount;
+    });
+  };
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount;
+    });
+  };
   return (
     <Wrapper>
       <div className="colors">
