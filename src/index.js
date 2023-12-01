ee23b91a6fe2dfdc997894f26b41167823d168ef
Aida <aidasufaj00@gmail.com>
2023-12-01 15:44:22 +0100

    user context setup

--- src/index.js
+++ src/index.js
@@ -9,15 +9,29 @@ import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+//configure Auth0Provider
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //place the FilterProvider inside  of the ProductsProvider
 
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="dev-lwjue4h2qjs3hdlg.us.auth0.com"
+    clientId="BiLUsw8DvQXHHsZbgkDBLvoVzx3ztk6i"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
+
+//dev-lwjue4h2qjs3hdlg.us.auth0.com
+//rt7kT2lm1huFEunexFhioElEIyzPChz4
